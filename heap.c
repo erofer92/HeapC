8275dc1a9954c42a9a4ed08edf89539b10ecef65
Eron <erofer92@gmail.com>
2017-06-23 00:41:32 -0300

    adjusts...

--- heap.c
+++ heap.c
@@ -10,22 +10,22 @@ struct heap
 	int top;
 };
 
-Heap* heap_create(void)
+Heap* heap__create(void)
 {
 	Heap *heap = (Heap*) malloc(sizeof(Heap));
-    heap->top = 0;
+	heap->top = 0;
 	return heap;
 }
 
-void heap_destroy(Heap **heap)
+void heap__destroy(Heap **heap)
 {
 	if (*heap != NULL)
-        free(*heap);
-	
-    *heap = NULL;
+		free(*heap);
+
+	*heap = NULL;
 }
 
-void heap_push(Heap *heap, int value)
+void heap__push(Heap *heap, int value)
 {
 	if (heap->top == MAX_SIZE)
 		return;
@@ -33,47 +33,47 @@ void heap_push(Heap *heap, int value)
 	heap->items[heap->top++] = value;
 }
 
-int heap_pop(Heap *heap)
+int heap__pop(Heap *heap)
 {
 	return heap->top ? heap->items[--heap->top] : -1;
 }
 
-int heap_size(Heap *heap)
+int heap__size(Heap *heap)
 {
 	return heap->top;
 }
 
-int heap_top(Heap *heap)
+int heap__top(Heap *heap)
 {
-	return heap->top ? heap->items[heap->top-1] : -1;
+	return heap->top ? heap->items[heap->top - 1] : -1;
 }
 
-void heap_print(Heap *heap)
+void heap__print(Heap *heap)
 {
 	printf("[ ");
 
-	for(size_t i = 0; i < heap->top; i++)
+	for (size_t i = 0; i < heap->top; i++)
 		printf("%d ", heap->items[i]);
 
 	printf("]\n");
 }
 
-Heap* heap_clone(Heap *heap)
+Heap* heap__clone(Heap *heap)
 {
-    Heap *new_heap = heap_create();
+	Heap *new_heap = heap__create();
 
 	for (size_t i = 0; i < heap->top; i++)
 		new_heap[i] = heap[i];
 
 	new_heap->top = heap->top;
-	
+
 	return new_heap;
 }
 
-void heap_reverse(Heap *heap)
+void heap__reverse(Heap *heap)
 {
 	for (size_t i = 0, j = heap->top - 1; i < j; i++, j--)
-    {
+	{
 		int tmp = heap->items[i];
 		heap->items[i] = heap->items[j];
 		heap->items[j] = tmp;
--- heap.h
+++ heap.h
@@ -4,54 +4,45 @@
 
 typedef struct heap Heap;
 
-extern
-Heap* heap_create(void);
+extern Heap* heap__create(void);
 
-extern
-void heap_destroy(Heap **heap);
+extern void heap__destroy(Heap **heap);
 
-extern
-void heap_push(Heap *heap, int value);
+extern void heap__push(Heap *heap, int value);
 
-extern
-int heap_pop(Heap *heap);
+extern int heap__pop(Heap *heap);
 
-extern
-int heap_top(Heap *heap);
+extern int heap__top(Heap *heap);
 
-extern
-int heap_size(Heap *heap);
+extern int heap__size(Heap *heap);
 
-extern
-Heap* heap_clone(Heap *heap);
+extern Heap* heap__clone(Heap *heap);
 
-extern
-void heap_reverse(Heap *heap);
+extern void heap__reverse(Heap *heap);
 
-extern
-void heap_print(Heap *heap);
+extern void heap__print(Heap *heap);
 
 static const struct
 {
-    Heap* (*create) (void);
-    void (*destroy) (Heap **heap);
-    void (*push) (Heap *heap, int value);
-    int (*pop) (Heap *heap);
-    int (*top) (Heap *heap);
-    int (*size) (Heap *heap);
-    Heap* (*clone) (Heap *heap);
-    void (*reverse) (Heap *heap);
-    void (*print) (Heap *heap);
+    Heap* (*create)(void);
+    void (*destroy)(Heap **heap);
+    void (*push)(Heap *heap, int value);
+    int (*pop)(Heap *heap);
+    int (*top)(Heap *heap);
+    int (*size)(Heap *heap);
+    Heap* (*clone)(Heap *heap);
+    void (*reverse)(Heap *heap);
+    void (*print)(Heap *heap);
 } heap = {
-    .create = heap_create,
-    .destroy = heap_destroy,
-    .push = heap_push,
-    .pop = heap_pop,
-    .top = heap_top,
-    .size = heap_size,
-    .clone = heap_clone,
-    .reverse = heap_reverse,
-    .print = heap_print
+    .create = heap__create,
+    .destroy = heap__destroy,
+    .push = heap__push,
+    .pop = heap__pop,
+    .top = heap__top,
+    .size = heap__size,
+    .clone = heap__clone,
+    .reverse = heap__reverse,
+    .print = heap__print
 };
 
 #endif // HEAP_H
